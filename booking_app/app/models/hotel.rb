062a3609910be63dae38043c688d11c81b4cfee7
chudziutkaa <klaudiachudy94@gmail.com>
2016-05-11 11:04:13 +0200

    create hotel model
    add associations, validations and browse method

---
+++ booking_app/app/models/hotel.rb
@@ -0,0 +1,21 @@
+class Hotel < ActiveRecord::Base
+	has_many :rooms
+	has_many :orders, through: :rooms
+	has_many :comments
+	has_many :prices
+	has_one :gallery
+	has_many :photos, through: :gallery
+
+	validates :name, length: { minimum: 3 }
+	validates :standard, inclusion: { in: %w(* ** *** **** *****), message: "If you don't know standard of your hotel it would be better if you closed it :(" }
+	validates :description, length: { in: 20..800 }
+	validates :location, length: { minimum: 5 }
+
+	def self.browse(search)
+		if search
+			Hotel.where('name LIKE ?', "%#{search}%")
+		else
+			Hotel.all
+		end
+	end
+end
