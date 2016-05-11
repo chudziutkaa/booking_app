8aeec7c298650be8dff116e1a46c64ce011dd09c
chudziutkaa <klaudiachudy94@gmail.com>
2016-05-11 11:15:20 +0200

    Revert "create orders table"
    This reverts commit b9684907106c21f7529fe551eb83b6beac1ccb39.

--- booking_app/db/migrate/20160324175337_create_table_order.rb
+++
@@ -1,9 +0,0 @@
-class CreateTableOrder < ActiveRecord::Migration
-  def change
-    create_table :table_orders do |t|
-    	t.references :room
-    	t.datetime :start_time
-    	t.datetime :end_time
-    end
-  end
-end
